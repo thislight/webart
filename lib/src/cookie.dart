f42c0c85456a48e1f4b63a55bbd06862eb1ee4b1
thisLight <l1589002388@gmail.com>
2017-08-10 16:04:49 +0800

    Add document

--- lib/src/cookie.dart
+++ lib/src/cookie.dart
@@ -14,6 +14,10 @@ final FunctionalLayer CookieLayer = new FunctionalLayer((Request req, Go go){
     ..setName("CookieLayer");
 
 
+/// Plugin for cookie support
+/// You can get cookies from "cookies" in [Context], for example `request.context("cookies")`
+/// But this type is [Map<String, Cookie>] not [Map<String, String>]
+/// the key of this cookie plugin implements by [shelf_redis_session_store](https://pub.dartlang.org/packages/shelf_redis_session_store)
 class CookiePlugin extends Plugin{
     
     @override
