dbaffedbabd9fd98770e5b99f2d59a81f8c8fc03
thisLight <l1589002388@gmail.com>
2017-08-11 20:58:31 +0800

    Remove unused import

--- lib/src/web.dart
+++ lib/src/web.dart
@@ -8,7 +8,6 @@ import "./request.dart" show Request;
 import "./plugin.dart" show Plugin;
 import "./route.dart" show Router,RouteSpec;
 import "cookie.dart" show CookiePlugin;
-import "session.dart" show SessionPlugin;
 import "package:shelf/shelf.dart" as shelf;
 import "package:shelf/shelf_io.dart" as io;
 import "package:logging/logging.dart" show Logger;
