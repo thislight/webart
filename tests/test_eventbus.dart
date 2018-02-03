979eca9d5a3b360b9058cdedcde05bd48914f438
thislight <l1589002388@gmail.com>
2018-02-03 20:15:18 +0800

    Fix problem

--- tests/test_eventbus.dart
+++ tests/test_eventbus.dart
@@ -7,29 +7,22 @@ import 'package:test/test.dart';
 import 'package:webart/src/plugin.dart' show EventBus;
 
 
-class EventBusTestCase{
-    void doTest(){
-        group('EventBus', () {
-            tearDown((){
-                EventBus.renew();
-            });
-            test('can listen and happen event', () {
-                var p = false;
-                EventBus.on("testEvent").then(() => p = true);
-                EventBus.happen("testEvent", []);
-                expect(p, equals(true));
-            });
-            test('newEventHandlerWillBeAdded event will happen when add new event',(){
-                var p = false;
-                EventBus.on('eventbus.newEventHandlerWillBeAdded').then(() => p = true);
-                EventBus.on('testEvent').then(() => p);
-                expect(p, equals(true));
-            });
-        });
-    }
-}
-
-
 void main(){
-    new EventBusTestCase().doTest();
+    group('EventBus', () {
+        tearDown((){
+            EventBus.renew();
+        });
+        test('can listen and happen event', () {
+            var p = false;
+            EventBus.on("testEvent").then(() => p = true);
+            EventBus.happen("testEvent", []);
+              expect(p, equals(true));
+          });
+          test('newEventHandlerWillBeAdded event will happen when add new event',(){
+              var p = false;
+              EventBus.on('eventbus.newEventHandlerWillBeAdded').then(() => p = true);
+             EventBus.on('testEvent').then(() => p);
+             expect(p, equals(true));
+          });
+      });
 }
