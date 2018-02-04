b0b64238a2e7aab3666e4654d421149de5bf57e7
thislight <l1589002388@gmail.com>
2018-02-04 17:51:05 +0800

    Upgrade packages & Fix could not add new event handler error

--- lib/src/plugin.dart
+++ lib/src/plugin.dart
@@ -7,57 +7,75 @@ abstract class Plugin{
 }
 
 
-typedef void _OnGotFunctionHandler(Function f);
+typedef void _OnGotFunctionHandler(String event,EventHandler f);
+
+
+typedef List<dynamic> EventHandler(List<dynamic> args);
 
 
 class _FutureEvent{
     _OnGotFunctionHandler _onGotFunctionHandler;
-    _FutureEvent(this._onGotFunctionHandler);
+    String _event;
+    _FutureEvent(this._event, this._onGotFunctionHandler);
 
-    Future then(Function f) async => await this._onGotFunctionHandler(f);
+    Future then(Function f) async => await this._onGotFunctionHandler(_event, f);
 }
 
 
-class EventBus{
-    static final Map<String,List<Function>> _map = new Map();
+class _EventBus{
+    Map<String,List<EventHandler>> _map;
 
-    static _FutureEvent on(String event){
-        if (_map[event]==null){
-            _map[event] = new List();
-        }
-        return new _FutureEvent((f) async{
-            List p = await happen("eventbus.newEventHandlerWillBeAdded",[event,f],defReturn: [null,f]);
-            _map[event].add(p[1]);
+    _EventBus(){
+        _map = {};
+    }
+
+    _FutureEvent on(String event){
+        return new _FutureEvent(event,(String e,EventHandler f){
+            if (_map[e]==null){
+                _map[e] = [];
+            }
+            happen("eventbus.newEventHandlerWillBeAdded",[event,f]);
+            _map[e].add(f);
+            print(_map);
         });
     }
 
-    static dynamic _happen(String event,List args,{defReturn: null}) async{
+    dynamic _happen(String event,List args,{defReturn: null}) async{
         if (_map[event]==null) return defReturn;
         if (args == null) args = [];
-        for (Function f in _map[event]){
+        for (EventHandler f in _map[event]){
             args = await f(args);
         }
         return args;
     }
 
-    static dynamic happen(String event,List args,{dynamic defReturn: null}) async{
+    dynamic happen(String event,List args,{dynamic defReturn: null}) async{
         var v = await _happen(event, args,defReturn: defReturn);
         await _happen("eventbus.newEventHappened", [event]);
         return v;
     }
 
-    static renew(){
+    void renew(){
         _map.clear();
     }
     
-    static printInfo(){
+    void printInfo(){
         print("--- Raw Event Bus Output ---");
         print("Current event bus has ${_map.keys.length} event.");
         _map.forEach((k,v){
-            print(k);
-            v.forEach((val) => print(v));
+            print("$k:");
+            v.forEach((val) => print("  $v"));
         });
         print("--- Output End ---");
     }
     
 }
+
+
+final _EventBus EventBus = new _EventBus();
+
+
+dynamic originBack(Function h) => (List args) async{
+    await h(args);
+    return args;
+};
--- tests/eventbus_test.dart
+++ tests/eventbus_test.dart
@@ -4,7 +4,7 @@ import 'package:test/test.dart';
 @TestOn('vm')
 
 
-import 'package:webart/src/plugin.dart' show EventBus;
+import 'package:webart/src/plugin.dart' show EventBus,originBack;
 
 
 void main(){
@@ -15,14 +15,14 @@ void main(){
         });
         test('can listen and happen event', () {
             var p = false;
-            EventBus.on("testEvent").then(() => p = true);
+            EventBus.on("testEvent").then(originBack((_) async => p = true));
             EventBus.happen("testEvent", []);
               expect(p, equals(true));
           });
           test('newEventHandlerWillBeAdded event will happen when add new event',(){
               var p = false;
-              EventBus.on('eventbus.newEventHandlerWillBeAdded').then(() => p = true);
-             EventBus.on('testEvent').then(() => p);
+              EventBus.on('eventbus.newEventHandlerWillBeAdded').then(originBack((_) => p = true));
+             EventBus.on('testEvent').then(originBack((_) => p));
              expect(p, equals(true));
           });
       });
